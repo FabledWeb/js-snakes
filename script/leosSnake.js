bf5acd08d5bcda1b92d012b09484ed9acae78c44
Eddie Canales <eddie.canales@crosschx.com>
2019-11-25 21:10:19 -0500

    started on making it avoid "traps."

--- script/leosSnake.js
+++ script/leosSnake.js
@@ -1,14 +1,35 @@
 class LeosSnake extends Snake {
+  constructor() {
+    super({
+      color: "#d50000",
+      accent: "#f44336",
+      name: "FyreFyte",
+      author: "Leo"
+    });
+  }
 
-    constructor() {
-      super({
-        color: '#d50000',
-        accent: '#f44336',
-        name: 'FyreFyte',
-        author: 'Leo',
-      });
+  check2spotsahead(direction, opponent, dimensions) {
+    const newDirection = {
+      x: direction.x,
+      y: direction.y
+    };
+    const currentDirection = this.currentDirection();
+    const directions = Snake.DIRECTION;
+    if (currentDirection === directions.down) {
+      newDirection.y += 1;
+    } else if (currentDirection === directions.up) {
+      newDirection.y -= 1;
+    } else if (currentDirection === directions.right) {
+      newDirection.x += 1;
+    } else if (currentDirection === directions.left) {
+      newDirection.x -= 1;
     }
-
+    if (this.isValidMove(newDirection, opponent, dimensions)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   update(dt, opponent, dimensions) {
     // dt = time since last update
     // opponent = array of boxes owned by my opponent
@@ -22,19 +43,15 @@ class LeosSnake extends Snake {
     let move;
     if (this.body.length === 1) {
       move = this.moveUp();
-    }
-    else if (this.isValidMove(this.turnRight(), opponent, dimensions)) {
+    } else if (this.isValidMove(this.turnRight(), opponent, dimensions)) {
       move = this.turnRight();
-    }  
-    else if (this.isValidMove(this.goStraight(), opponent, dimensions)) {
+    } else if (this.check2spotsahead(this.goStraight(), opponent, dimensions)) {
       move = this.goStraight();
-    }
-    else {
+    } else {
       move = this.turnLeft();
-    }  
-    
+    }
+
     return move;
-    
   }
 }
 snakeClasses.push(LeosSnake);
