3e79f7cf6949945a559c5a1ab0ca5909fed9ad20
Eddie Canales <eddiecanales@gmail.com>
2019-11-26 07:49:54 -0500

    leave 1 square gaps

--- script/dadsSnake.js
+++ script/dadsSnake.js
@@ -4,6 +4,8 @@ class DadsSnake extends Snake {
   //   Fill as much space as you can without leaving any gaps by
   //   trying to turn left as much as you can until you have to turn right,
   //   then trying to turn right as much as you can... repeat.
+  //   New: also always turn 1 square away from obstacles, which leaves
+  //   many traps.
 
   constructor() {
     super({
@@ -13,8 +15,65 @@ class DadsSnake extends Snake {
       author: 'Dad',
     });
     this.favoredDirection = this.turnLeft();
-    this.directions = Snake.DIRECTION;
-    this.favoredTurnDirection = this.directions.left;
+    this.favoredTurnDirection = DIRECTION.left;
+  }
+
+  isStraightClearTwoSpaces(turnDirection, opponent, dimensions) {
+    const currentDirection = this.currentDirection();
+    const head = this.head();
+    let oneSpace;
+    let twoSpaces;
+    let newDirection = currentDirection;
+
+    // figure out which direction you will be facing after turning
+    if (turnDirection === DIRECTION.left) {
+      switch(currentDirection) {
+        case DIRECTION.up:
+          newDirection = DIRECTION.left;
+          break;
+        case DIRECTION.down:
+          newDirection = DIRECTION.right;
+          break;
+        case DIRECTION.left:
+          newDirection = DIRECTION.down;
+          break;
+        case DIRECTION.right:
+          newDirection = DIRECTION.up;
+          break;
+      }
+    } else if (turnDirection === DIRECTION.right) {
+      switch(currentDirection) {
+        case DIRECTION.up:
+          newDirection = DIRECTION.right;
+          break;
+        case DIRECTION.down:
+          newDirection = DIRECTION.left;
+          break;
+        case DIRECTION.left:
+          newDirection = DIRECTION.up;
+          break;
+        case DIRECTION.right:
+          newDirection = DIRECTION.right;
+          break;
+      }
+    }
+
+    if (newDirection === DIRECTION.up) {
+      oneSpace = {x: head.x, y: head.y-1};
+      twoSpaces = {x: head.x, y: head.y-2};
+    } else if (newDirection === DIRECTION.down) {
+      oneSpace = {x: head.x, y: head.y+1};
+      twoSpaces = {x: head.x, y: head.y+2};
+    } else if (newDirection === DIRECTION.left) {
+      oneSpace = {x: head.x-1, y: head.y};
+      twoSpaces = {x: head.x-2, y: head.y};
+    } else {
+      oneSpace = {x: head.x+1, y: head.y};
+      twoSpaces = {x: head.x+2, y: head.y};
+    }
+    // check both the first space in this direction and the one after that
+    return this.isValidMove(oneSpace, opponent, dimensions)
+          && this.isValidMove(twoSpaces, opponent, dimensions);
   }
 
   update(dt, opponent, dimensions) {
@@ -25,7 +84,7 @@ class DadsSnake extends Snake {
 
     if (this.body.length === 1) {
       this.favoredDirection = this.turnLeft();
-      this.favoredTurnDirection = this.directions.left;
+      this.favoredTurnDirection = DIRECTION.left;
     }
 
     let move = this.moveUp();
@@ -41,26 +100,30 @@ class DadsSnake extends Snake {
       } else {
         move = this.moveRight();
       }
-    } else if (this.favoredTurnDirection === this.directions.left) {
+    } else if (this.favoredTurnDirection === DIRECTION.left) {
       // prefer to keep turning left
       if (this.isValidMove(this.turnLeft(), opponent, dimensions)) {
         move = this.turnLeft();
-      } else if (this.isValidMove(this.goStraight(), opponent, dimensions)) {
+      } else if (this.isStraightClearTwoSpaces(DIRECTION.none, opponent, dimensions)) {
         move = this.goStraight();
-      } else {
+      } else if (this.isValidMove(this.turnRight(), opponent, dimensions)) {
         move = this.turnRight();
-        this.favoredTurnDirection = this.directions.right;
+        this.favoredTurnDirection = DIRECTION.right;
+      } else {
+        move = this.goStraight();
       }
     }
     else {
       // prefer to keep turning right
       if (this.isValidMove(this.turnRight(), opponent, dimensions)) {
         move = this.turnRight();
-      } else if (this.isValidMove(this.goStraight(), opponent, dimensions)) {
+      } else if (this.isStraightClearTwoSpaces(DIRECTION.none, opponent, dimensions)) {
         move = this.goStraight();
-      } else {
+      } else if (this.isValidMove(this.turnLeft(), opponent, dimensions)) {
         move = this.turnLeft();
-        this.favoredTurnDirection = this.directions.left;
+        this.favoredTurnDirection = DIRECTION.left;
+      } else {
+        move = this.goStraight();
       }
     }
 
